27989759b528daccebc6bc8a9b0fffa9bf9a6cb1
Mehdi Rezaie <medirz90@icloud.com>
2020-03-20 12:17:13 -0400

    add bash to make movie

---
+++ CoronaVis/makemovie.bash
@@ -0,0 +1,25 @@
+#------------------------------------------------------
+#   
+#   Shell script to make a movie
+#   (c) Katebi&Rezaie Co.
+#   
+#   Steps
+#           1. make frames in png
+#           2. use ffmpeg to combine them into mp4
+#
+#------------------------------------------------------
+
+# Choose the theme: options are `dark` or `light`
+theme=dark
+echo "theme: "${theme}
+
+# Run app.py
+python app.py -m -t ${theme} 
+
+# Run ffmpeg
+dir=${PWD}/frames
+output_name=Covid19_${theme}.mp4
+echo "movie : "${output_name}
+
+echo "change to "${dir}
+ffmpeg -pattern_type glob -framerate 6 -i ${theme}_"**.png" -c:v h264 -r 10 -s 1920x1080 -pix_fmt yuv420p ${output_name}
